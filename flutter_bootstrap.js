803230ef65daeb573892a5cd242651fb0c37aecc
aminhm <iqson.amin@gmail.com>
2026-04-04 18:30:24 +0300

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2221042162"
+    serviceWorkerVersion: "2193423014"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b86ba0d68fc521d26bd95d4f5a8974db",
+const RESOURCES = {"flutter_bootstrap.js": "01880607b39d79cf049dfdb9a75e6725",
 "version.json": "317e06b74ed720e2d91ef10154820dad",
 "index.html": "3b88c1db747d4444ab044f4d15151696",
 "/": "3b88c1db747d4444ab044f4d15151696",
-"main.dart.js": "b7fd0d11c7ec9b9214ab93b419e0193b",
+"main.dart.js": "063d59b6a8ac0ec2e56fd643d4a839c1",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
